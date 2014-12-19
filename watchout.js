86b2bd6bb07c6b99b5791330b124dd75400990bf
minocys <harich.leung@gmail.com>
2014-12-19 16:59:25 -0500

    created svg board, 1st asteroid!

--- watchout.js
+++ watchout.js
@@ -1,2 +1,12 @@
 // start slingin' some d3 here.
 
+var svg = d3.select('body').append('svg')
+          .classed('board', true);
+
+svg.append('svg')
+  .append('circle')
+  .attr('cx', 30)
+  .attr('cy', 30)
+  .attr('r', 30)
+  .style('fill', 'purple')
+  .classed('assteroid', true);
