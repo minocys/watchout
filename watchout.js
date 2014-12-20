c70a00bd6ed09d26c7602be2eb2866bef84133ef
minocys <harich.leung@gmail.com>
2014-12-20 15:25:40 -0500

    completed scoreboard, enemy and player movement

--- watchout.js
+++ watchout.js
@@ -1,31 +1,45 @@
 //Your code here
+var collisionCount = 0;
+var score = 0;
+var highScore = d3.select('.high').select('span');
+var collisions = d3.select('.collisions').select('span');
+var currentScore = d3.select('.current').select('span');
+var enemies = 20;
 
-//create random coordinates for enemies
-
-var enemies = 40;
-var populateEnemies = function(enemies){
+//utility to generate array of objects with coordinates as values
+var makeCoords = function(number){
   var coordinates = [];//an array of objects with x:y key/value
-  for(var i = 0; i < enemies; i++){
+  for(var i = 0; i < number; i++){
     var x = Math.floor(Math.random()*(window.innerWidth-100));
     var y = Math.floor(Math.random()*(window.innerHeight-100));
     var obj = {'x':x, 'y':y};
     coordinates.push(obj);
   }
   return coordinates;
-}
+};
 
+//create game board
 var gameBoard = d3.select('body')
                 .append('svg')
-                .classed('board', true);
+                .classed('gameboard', true);
 
 //create enemy
 gameBoard.selectAll('svg')
-        .data(populateEnemies(enemies)) //storage needs to hold an object of all enemies to appear on board
+        .data(makeCoords(enemies)) //storage needs to hold an object of all enemies to appear on board
         .enter()
         .append('svg')
+        .classed('enemy', true)
         .attr({
           x: function(d){return Number(d.x)},
           y: function(d){return Number(d.y)}
+        })
+        .on('mouseover', function(){
+          collisionCount++;
+          collisions.text(collisionCount);
+          if( score > highScore.text() ){
+            highScore.text(score);
+          }
+          score = 0;
         })
         .append('circle')
         .attr('cx', 20)
@@ -33,10 +47,40 @@ gameBoard.selectAll('svg')
         .attr('r', 20)
         .style('fill', 'purple');
 
-//populateEnemies(50);
+//Enemy movement - interval = time between each move, duration = speed of objects
+setInterval(function(){
+  gameBoard.selectAll('.enemy')
+        .data(makeCoords(enemies))
+        .transition()
+        .duration(1850)
+        .attr({
+          x: function(d){return Number(d.x)},
+          y: function(d){return Number(d.y)}
+        });
+}, 1000);
 
-// var movement = function
-// svg.selectAll('.assteroid')
-// .transition()
-// .attr('x', x)
-// .attr('y', y);
+//create character/player
+var player = gameBoard.append('svg:ellipse')
+        .classed('player', true)
+        .attr('cx', 20)
+        .attr('cy', 20)
+        .attr('rx', 20)
+        .attr('ry', 10)
+        .style('fill', 'blue');
+
+//attach player object to player mouse
+gameBoard.on('mousemove', function(d){
+    var x = d3.mouse(this)[0];
+    var y = d3.mouse(this)[1];
+    player.attr("cx", x)
+          .attr("cy", y);
+});
+
+//score keeper
+setInterval( function() {
+  score++;
+  currentScore.text(score);
+}, 40);
+
+
+
